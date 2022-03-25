714b19b8104afdffd7f0f9350a23738ec9cc18e8
clarissa gonzales <clarissa.gonzales@gmail.com>
2022-03-24 21:55:13 -0500

    do it

--- pageObjects.ts
+++ pageObjects.ts
@@ -1,1 +1,38 @@
-// to be determined
+import {By} from "selenium-webdriver"
+import { BasePage } from "./basePage"
+
+export class WYZE extends BasePage {
+
+    //Wyze selectors
+    shopNow: By = By.xpath(`(//a[@href="/collections/spring-sale"])[3]`)
+
+    headPhones: By = By.xpath(`(//a[@href="/products/wyze-buds"])[3]`)
+    
+    //cartIcon: By = By.xpath
+
+    addToCart: By = By.xpath('(//button[@class="btn-gr-solid-l button--full-width"])[1]')
+
+    //yourCart: By = By.xpath(`//h1[@class="title title--primary"]`)
+     
+    //quantityButton: By = By.xpath(`//button[@class="quantity__button no-js-hidden"]`)
+    
+    checkOut: By = By.xpath(`(//button[@class="btn-gr-solid-l button--full-width"])[2]`)
+      
+    //showOrderSummary: By = By.xpath(`//span[@class="order-summary-toggle__text order-summary-toggle__text--show"]`)
+    
+    returnToCart: By = By.xpath(`//span[@class="step__footer__previous-link-content"]`)
+    
+    removeFromCart: By = By.xpath(`//a[@class="button button--tertiary"]`)
+
+    aboutFooter: By = By.xpath(`//a[@href="/pages/our-story"]`)
+
+    thumbnailfromYouTube: By = By.xpath(`//div[@class="ytp-cued-thumbnail-overlay-image"]`)
+    
+    iconYouTube: By = By.xpath(`//a[@href="https://www.youtube.com/wyzelabs"]`)
+
+
+
+constructor() {
+        super({url: "https://www.wyze.com/"});
+    }
+}
--- wyzetest.test.ts
+++ wyzetest.test.ts
@@ -1,1 +1,32 @@
-//wahoo
+import {WYZE} from "./pageObjects"
+
+
+const wyze = new WYZE
+
+test("Add earbuds to Cart then Remove.", async () => {
+    await wyze.navigate();
+    await wyze.driver.manage().window().maximize(); 
+    await wyze.click(wyze.shopNow);
+    await wyze.click(wyze.headPhones);
+    await wyze.driver.sleep(3000);
+    await wyze.click(wyze.addToCart);
+    await wyze.driver.sleep(3000);
+    await wyze.click(wyze.checkOut);
+    await wyze.driver.sleep(3000);
+    await wyze.click(wyze.returnToCart);
+    await wyze.click(wyze.removeFromCart);
+    await wyze.driver.sleep(10000);
+})
+
+
+test("Check Youtube icon.", async () => {
+    await wyze.click(wyze.aboutFooter);
+    //await wyze.find(wyze.thumbnailfromYouTube);
+   //await wyze.getElement(wyze.thumbnailfromYouTube)
+    await wyze.click(wyze.iconYouTube);
+    await wyze.driver.sleep(3000);
+    // Navigates to YouTube 404 channel
+    await wyze.driver.quit();
+})
+
+
